a97d3726824b175625685a063e06f82f061bcb4d
Max Zhao <mdcw1103@gmail.com>
2022-11-04 12:18:43 +0800

    chore: add reference

--- questions/00011-easy-tuple-to-object/template.ts
+++ questions/00011-easy-tuple-to-object/template.ts
@@ -1,4 +1,7 @@
 // https://github.com/type-challenges/type-challenges/tree/main/questions/00011-easy-tuple-to-object
+
+// https://www.typescriptlang.org/docs/handbook/2/indexed-access-types.html
+// https://stackoverflow.com/questions/59187941/whats-the-tnumber-mean-in-typescript-code
 type TupleToObject<T extends readonly (string | number)[]> = {
   [P in T[number]]: P
 }
